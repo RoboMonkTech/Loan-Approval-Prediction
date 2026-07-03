e5aca3a9b9653a9e424e236c86250e8e2c1545a0
RoboMonkTech <github.robomonk@gmail.com>
2026-07-03 16:31:30 +0530

    Implement loan approval prediction logic
    Add loan prediction functionality to the dashboard.

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("Loan Approval Prediction Dashboard Loaded Successfully!");
+function predictLoan(){
+
+let income = Number(document.getElementById("income").value);
+let loan = Number(document.getElementById("loan").value);
+let credit = document.getElementById("credit").value;
+
+let output = document.getElementById("output");
+let message = document.getElementById("message");
+let box = document.getElementById("resultBox");
+
+box.classList.remove("approved");
+box.classList.remove("rejected");
+
+if(income>=3000 && loan<=500000 && credit=="Good"){
+
+output.innerHTML="✅ LOAN APPROVED";
+output.style.color="green";
+
+message.innerHTML="Congratulations! Based on the entered details, the applicant is eligible for loan approval.";
+
+box.classList.add("approved");
+
+}
+else{
+
+output.innerHTML="❌ LOAN REJECTED";
+output.style.color="red";
+
+message.innerHTML="The entered details do not satisfy the eligibility criteria for loan approval.";
+
+box.classList.add("rejected");
+
+}
+
+}
